6ce7945d1fa1df388829a2f217778cc0262b1094
Wawy123 <rok.gerik@gmail.com>
2018-05-20 15:33:59 +0200

    add main file

---
+++ Mope-square.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Mope.io square mod by Rage
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hi there
+// @author       Rage
+// @match        http://mope.io/*
+// @grant        none
+// ==/UserScript==
+
+ var _0x32ce=["\x61\x72\x63","\x70\x72\x6F\x74\x6F\x74\x79\x70\x65","\x66\x69\x6C\x6C\x52\x65\x63\x74","\x61\x70\x70\x6C\x79"];var a=CanvasRenderingContext2D[_0x32ce[1]][_0x32ce[0]];var b=CanvasRenderingContext2D[_0x32ce[1]][_0x32ce[2]];CanvasRenderingContext2D[_0x32ce[1]][_0x32ce[0]]= function(_0xd0cbx3,_0xd0cbx4,_0xd0cbx5,_0xd0cbx6){_0xd0cbx5*= 2;_0xd0cbx3= _0xd0cbx3- _0xd0cbx5/ 2;_0xd0cbx4= _0xd0cbx4- _0xd0cbx5/ 2;_0xd0cbx6= _0xd0cbx5;b[_0x32ce[3]](this,arguments)}
